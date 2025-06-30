608b6fb3357b1b262523010d428f81384b9515d6
igalwini <igush2001!>
2025-06-30 18:10:53 -0300

    avances

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from app.models import QuestionRequest, AnswerResponse
 from app.rag_pipeline import RAGPipeline
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 rag = RAGPipeline()
+
+# Habilitar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/ask", response_model=AnswerResponse)
 def ask_question(req: QuestionRequest) -> AnswerResponse:
